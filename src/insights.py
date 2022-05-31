30ce390382a92598f73a48ef4afd5f8e5cf1ce02
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-05-31 19:24:15 -0300

    feat: implements function get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -56,7 +56,12 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs = read(path)
+    industries = set()
+    for job in jobs:
+        if job["industry"]:
+            industries.add(job["industry"])
+    return list(industries)
 
 
 def filter_by_industry(jobs, industry):
