e38f82375d325d5372022aecb9ecb4278529c843
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-06-05 16:36:37 -0300

    test: implements test to the function read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    dict_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert 'titulo' not in dict_jobs[0]
+    assert 'salario' not in dict_jobs[0]
+    assert 'tipo' not in dict_jobs[0]
+    assert 'title' in dict_jobs[0]
+    assert 'salary' in dict_jobs[0]
+    assert 'type' in dict_jobs[0]
